667713559e4b8738d7cb99f0bf2d2beff995c18e
M Groesbeck <m.groesbeck@gmail.com>
2020-08-13 12:37:11 -0700

    Currently running infinite loop; will debug new code or uncomment old.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from adv_utils import get_route, Scout
+from adv_utils import Scout
 
 import random
 from ast import literal_eval
@@ -29,39 +29,19 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 
-# Version 1: traveling blind
-# Algorithm refers only to rooms adjacent to known rooms
-# Keep dict or set (x) of known rooms with unknown adjacent rooms
-# Start facing north
-# While not all rooms discovered:
-## If unknown room ahead:
-### Step forward
-### Add current room to known graph
-### Turn left
-## Else:
-### turn right until unknown room ahead or have turned 4 times
-### If no adjacent unknown rooms:
-#### Follow shortest path to a room in dict/set (x)
-# Additional details:
-## Keep track of coordinates relative to starting point:
-### If adjacent rooms with a connection have been visited, fill in connections
-## room.x and room.y are coordinates
-### going east increases x, going north increases y
-### so keep a dict of rooms by (x,y)
-
-# Version 2: use your map
-# Keep dict/set (x) of rooms with unvisited adjacents
-## True if visited *or no room*, False if unvisited
-# Follow step-and-turn as above
-## At each step, update (x) as well as visited set and path
-# If no adjacent unvisited rooms, use world object graph
-## Find shortest path to an unvisited room
 traversal_path = []
-scout = Scout(world.starting_room, world)
-while len(scout.visited) < len(room_graph):
-    scout.automap()
-    scout.go_to_new()
-traversal_path = scout.steps
+
+scouts = [Scout(world.starting_room, world, i) for i in (True, False)]
+for scout in scouts:
+    while len(scout.visited) < len(room_graph):
+        # Transit continguous unexplored area:
+        scout.automap()
+        # Move to nearest unexplored room:
+        scout.go_to_new()
+print("L: ", len(scouts[0].steps))
+print("R: ", len(scouts[1].steps))
+traversals = sorted([scout.steps for scout in scouts], key=lambda traversal: len(traversal))
+traversal_path = traversals[0]
 
 # TRAVERSAL TEST
 visited_rooms = set()
--- projects/adventure/adv_utils.py
+++ projects/adventure/adv_utils.py
@@ -4,9 +4,10 @@ from collections import deque
 # Scout object to explore/traverse map:
 # Takes room object, not room id
 class Scout:
-    def __init__(self, room, world):
+    def __init__(self, room, world, lefty=True):
         self.room = room
         self.world = world
+        self.lefty = lefty
         self.compass = ["n", "e", "s", "w"]
         # facing: [0,1,2,3] = ["n", "e", "s", "w"]
         self.facing = 0
@@ -19,28 +20,75 @@ class Scout:
     def turn_right(self):
         self.facing = (self.facing + 1) % 4
     def check_new_adjacent(self):
+        # Returns True if any adjacent rooms have not been visited
         for exit in self.room.get_exits():
             if self.room.get_room_in_direction(exit).id not in self.visited:
                 return True
         return False
     def step_forward(self):
+        # Adds step to path, moves in facing direction, updates room.
         direction = self.compass[self.facing]
         if direction in self.room.get_exits():
             self.steps.append(direction)
             self.room = self.room.get_room_in_direction(direction)
             self.visited.add(self.room.id)
     def move_direction(self, direction):
+        # Moves without changing facing direction
         if direction in self.room.get_exits():
             self.steps.append(direction)
             self.room = self.room.get_room_in_direction(direction)
             self.visited.add(self.room.id)
+    def branch_eval(self, room):
+        checked = set([room.id])
+        branch_deque = deque([room.id])
+        while len(branch_deque) > 0:
+            this_room_id = branch_deque.popleft()
+            this_room = self.world.rooms[this_room_id]
+            if this_room_id not in checked and this_room_id not in self.visited:
+                checked.add(this_room_id)
+            for exit in this_room.get_exits():
+                new_room_id = this_room.get_room_in_direction(exit).id
+                if new_room_id not in checked and new_room_id not in self.visited:
+                    branch_deque.append(new_room_id)
+        return len(checked)
     def automap(self):
+        step_tens = 1
         while self.check_new_adjacent():
-            self.turn_left()
-            while self.compass[self.facing] not in self.room.get_exits():
+            if self.lefty:
+                self.turn_left()
+            else:
                 self.turn_right()
-            self.step_forward()
+            # while self.compass[self.facing] not in self.room.get_exits():
+            #     if self.lefty:
+            #         self.turn_right()
+            #     else:
+            #         self.turn_left()
+            # self.step_forward()
+            branches = {}
+            shortest = None
+            for exit in self.room.get_exits():
+                print(f"R: {self.room.id}, e: {exit}")
+                branches[exit] = self.branch_eval(self.room.get_room_in_direction(exit))
+            if self.lefty:
+                rel_dir = [0,1,2]
+            else:
+                rel_dir = [0,-1,-2]
+            for d in rel_dir:
+                this_dir = self.compass[(self.facing + d) % 4]
+                print(f"t_d: {this_dir}, s.f: {(self.facing + d) % 4}")
+                print(f"Br: {branches}")
+                print(f"Sh: {shortest}")
+                if this_dir in branches:
+                    print("In branches")
+                    if shortest:
+                        if branches[this_dir] < branches[shortest]:
+                            shortest = this_dir
+                    else:
+                        shortest = this_dir
+            self.facing = self.compass.index(shortest)
+            self.step_forward
     def nearest_new(self):
+        # Returns path to nearest unvisited room
         checked = set()
         room_deque = deque([[self.room.id, []]])
         while (len(room_deque) > 0):
@@ -60,6 +108,8 @@ class Scout:
     def go_to_new(self):
         for step in self.nearest_new():
             self.move_direction(step)
+        facing = self.compass.index(self.steps[-1])
+# End Scout object declaration
 
 # Shortest route between rooms, not restricted to already-mapped:
 def get_route(world, orig, dest):
